ce4ad3f8be5fcfd257d7a3bc0c425de860e3eb17
aniket.bhavsar <aniket.bhavsar@accionlabs.com>
2020-05-19 18:58:34 +0530

    function componets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Components/Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1> Hi, I am React Developer</h1>
+      <p> This is really Working </p>
+      <Person/>
+      <Person/>
+      <Person/>
+      <Person/>
       </div>
     );
   }
---
+++ src/Components/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = () =>{
+      return <p> I am From Person and I am {Math.floor((Math.random () * 20))} years Old</p>
+};
+
+export default person;
