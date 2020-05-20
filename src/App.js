51c0367b931d10f49f14eb028ab93e9c276a4e8e
aniket.bhavsar <aniket.bhavsar@accionlabs.com>
2020-05-20 19:33:33 +0530

    conditional Render

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
       { name: "ABD", age: 13 },
       { name: "Crone", age: 23 },
       { name: "Jhon", age: 53 }
-    ]
+    ],
+    showPersons: false
   };
 
   switchNameHandler = newName => {
@@ -32,42 +33,56 @@ class App extends Component {
     });
   };
 
+  togglePersonHandler = () => {
+    const currentShow = this.state.showPersons;
+    this.setState({ showPersons: !currentShow });
+  };
+
   render() {
     const style = {
-      backgroundColor:'yellow',
-      font:'inherit',
-      border:'1px solid red',
-      padding:'8px',
-      cursor:'pointer'
+      backgroundColor: "yellow",
+      font: "inherit",
+      border: "1px solid red",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (
+          <div>
+          <Person
+            name={this.state.person[0].name}
+            age={this.state.person[0].age}
+            switchs={this.switchNameHandler.bind(this, "Yahooo")}
+          >
+            <h1 style={{ color: "red" }}>This is child props</h1>
+          </Person>
+          <Person
+            name={this.state.person[1].name}
+            age={this.state.person[1].age}
+          />
+          <Person
+            name={this.state.person[2].name}
+            age={this.state.person[2].age}
+          />
+          <Person
+            name={this.state.person[3].name}
+            age={this.state.person[3].age}
+            change={this.nameChangeHandler}
+            tech={this.state.tech}
+          />
+        </div>
+      )
     }
+
     return (
       <div className="App">
         <h1> Hi, I am {this.state.tech} Developer</h1>
-        <button style = {style}
-        onClick={() => this.switchNameHandler("Bunyyyy")}>
-          Switch
+        <button style={style} onClick={this.togglePersonHandler}>
+          Toggle Name
         </button>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-          switchs={this.switchNameHandler.bind(this, "Yahooo")}
-        >
-          <h1 style={{ color: "red" }}>This is child props</h1>
-        </Person>
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-        />
-        <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age}
-        />
-        <Person
-          name={this.state.person[3].name}
-          age={this.state.person[3].age}
-          change={this.nameChangeHandler}
-          tech={this.state.tech}
-        />
+        {persons}
         <Student />
       </div>
     );
